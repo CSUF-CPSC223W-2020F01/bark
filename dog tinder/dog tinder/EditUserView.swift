8eca394c657335f9be7c23424e604f48586eab35
Blue B <kbayani@csu.fullerton.edu>
2020-10-09 15:47:45 -0700

    Iteration 1 commit
    Added Bottom navigation bar. Initialized empty views for the home and chat pages. User page contains a button that navigates to the "edit page" view

---
+++ dog tinder/dog tinder/EditUserView.swift
@@ -0,0 +1,20 @@
+//
+//  EditUserView.swift
+//  dog tinder
+//
+//  Created by Blue Bayani on 10/9/20.
+//
+
+import SwiftUI
+
+struct EditUserView: View {
+    var body: some View {
+        Text("Edit User Profile")
+    }
+}
+
+struct EditUserView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditUserView()
+    }
+}
--- dog tinder/dog tinder/HomeView.swift
+++ dog tinder/dog tinder/HomeView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text("Home View")
+        Text("Home")
     }
 }
 
