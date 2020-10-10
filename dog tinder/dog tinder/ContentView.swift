eb92b856d96769cbc9768da5b2ff710a4485898d
Blue B <kbayani@csu.fullerton.edu>
2020-10-09 17:40:28 -0700

    Iteration 1

--- dog tinder/dog tinder/ContentView.swift
+++ dog tinder/dog tinder/ContentView.swift
@@ -1,16 +1,23 @@
-//
-//  ContentView.swift
-//  dog tinder
-//
-//  Created by Blue Bayani on 10/8/20.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        //TabView displays the bottom navigation bar. (Home, Chat, and Profile)
+        //each .tabItem is clickable. when pressed, it will lead the user the the view indicated before the "."
+        TabView {
+            HomeView().tabItem {
+                Image(systemName: "house")
+                Text("Home")
+            }
+            ChatView().tabItem {
+                Image(systemName: "message")
+                Text("Chat")
+            }
+            UserView().tabItem {
+                Image(systemName: "person")
+                Text("Profile")
+            }
+        }
     }
 }
 
