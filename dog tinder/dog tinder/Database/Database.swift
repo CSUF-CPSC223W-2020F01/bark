097936fb8bafd77a9231ae2aadcc09bb247b9b8f
Derrick Lee <leederrick1517@gmail.com>
2020-12-03 04:06:51 -0800

    storage saving

--- dog tinder/dog tinder/Database/Database.swift
+++ dog tinder/dog tinder/Database/Database.swift
@@ -13,7 +13,6 @@ var husky: Dog = Dog(image: "husky", name: "Dot", breed: .Husky, gender: .male, 
 var bulldog: Dog = Dog(image: "englishbulldog", name: "Bobbie", breed: .EnglishBulldog, gender: .male, temperament: "Shy", size: .medium, weight: "50" , description: placeholderDesc)
 var goldie: Dog = Dog(image: "goldie", name: "Big Girl", breed: .GoldenRetriever, gender: .female, temperament: "Active", size: .large, weight: "65" , description: placeholderDesc)
 
-
 var user: User = User(firstName:"Miguel", lastName: "Lee", city: "Fontana", state: "California", email: "miguel@gmail.com", morning: true, afterNoon: true, night: true, listOfDogs: [pompom, husky])
 var user2: User = User(firstName:"Blue", lastName: "Bayani", city: "Fremon", state: "California", email: "blueb@gmail.com", morning: true, afterNoon: true, night: true, listOfDogs: [bulldog, goldie])
 
