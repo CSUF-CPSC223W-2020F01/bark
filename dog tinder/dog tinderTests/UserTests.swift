091d2bc38b76fb990cc694c2351c2b8876cd9481
MiguelAntonioMaciasMeza <miguel6021857@gmail.com>
2020-12-28 21:42:45 -0800

    Fixed User and Dog Unit test

--- dog tinder/dog tinderTests/UserTests.swift
+++ dog tinder/dog tinderTests/UserTests.swift
@@ -5,11 +5,10 @@
 //  Created by Derrick Lee on 10/12/20.
 //
 
-import XCTest
 @testable import dog_tinder
+import XCTest
 
 class UserTests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -29,14 +28,39 @@ class UserTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+
     func testUser() {
-        var user2: User = User(firstName:"Blue", lastName: "Bayani", city: "Fremon", state: "California", email: "blueb@gmail.com", listOfDogs: [bulldog, goldie])
+        let user2 = User(firstName: "Blue", lastName: "Bayani", city: "Fremon", state: "California", email: "blueb@gmail.com", listOfDogs: [bulldog, goldie])
         XCTAssertEqual(user2.firstName, "Blue")
         XCTAssertEqual(user2.lastName, "Bayani")
         XCTAssertEqual(user2.city, "Fremon")
         XCTAssertEqual(user2.state, "California")
         XCTAssertEqual(user2.email, "blueb@gmail.com")
-
     }
 
+    func testChangeName() {
+        let user2 = User(firstName: "Blue", lastName: "Bayani", city: "Fremon", state: "California", email: "blueb@gmail.com", listOfDogs: [bulldog, goldie])
+        user2.firstName = "NameChanged"
+        user2.lastName = "NameChanged"
+        XCTAssertEqual(user2.firstName, "NameChanged")
+        XCTAssertEqual(user2.lastName, "NameChanged")
+    }
+
+    func testChangeCity() {
+        let user2 = User(firstName: "Blue", lastName: "Bayani", city: "Fremon", state: "California", email: "blueb@gmail.com", listOfDogs: [bulldog, goldie])
+        user2.city = "CityChanged"
+        XCTAssertEqual(user2.city, "CityChanged")
+    }
+
+    func testChangeState() {
+        let user2 = User(firstName: "Blue", lastName: "Bayani", city: "Fremon", state: "California", email: "blueb@gmail.com", listOfDogs: [bulldog, goldie])
+        user2.state = "StateChanged"
+        XCTAssertEqual(user2.state, "StateChanged")
+    }
+
+    func testChangeEmail() {
+        let user2 = User(firstName: "Blue", lastName: "Bayani", city: "Fremon", state: "California", email: "blueb@gmail.com", listOfDogs: [bulldog, goldie])
+        user2.email = "EmailChanged"
+        XCTAssertEqual(user2.email, "EmailChanged")
+    }
 }
